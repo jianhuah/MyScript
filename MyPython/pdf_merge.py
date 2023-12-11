65fe7ff46f6c1be2d45045dece601e4f40a45688
Jesse Huang <jesse.huang@teradyne.com>
2023-12-11 12:19:48 +0800

    Python use PDF2 V3.0.0 to achieve multiple PDF files merge into single PDF file. VERFILED.

--- MyPython/pdf_merge.py
+++ MyPython/pdf_merge.py
@@ -1,9 +1,10 @@
 import os
-import time
-import pyautogui
+import PyPDF2
+##import pyautogui
 from pathlib import Path
+from PyPDF2 import PdfMerger
 from PyPDF2 import PdfWriter
-from PyPDF2 import PdfWriter
+from PyPDF2 import PdfReader, PdfWriter
 
 def get_files_from_folder(folder_path):
     folder_path = Path(folder_path)
@@ -18,22 +19,65 @@ def is_pdf_file(file_path):
     _, file_extension = os.path.splitext(file_path)
     return file_extension.lower() == '.pdf'
 
-# Specify the folder path
-folder_path = r'C:\Users\MI\Desktop\JH\通信原理_西电 课件全（江同学整理）\第1章 绪论'#'path/to/your/folder'
+def merge_pdfs_with_bookmarks(pdf_files, output_pdf):
+##    pdf_merger = PdfMerger()
+    pdf_merger = PdfWriter()
+    for index, pdf_file in enumerate(pdf_files):
+##        pdf_merger.append(pdf_file, bookmark=f'Bookmark {index + 1}')
+        print(pdf_file)
 
-# Get all files from the folder and its subfolders
-all_files = get_files_from_folder(folder_path)
+        pdf_reader = PdfReader(pdf_file)
+##        # Add bookmark for each PDF file
+##        outline_item = pdf_merger.add_outline_item(f'Bookmark {index + 1}', index)
+##        outline_item.append(pdf_merger.getPageDestination(index))
+          # Add bookmark for each PDF file
+##        print(index)
 
-merger = PdfWriter()
+        # Get the file name without the path
+        file_name_only = os.path.basename(pdf_file)
+        #destination_name = f'bookmark_{index + 1}_Start'
+        destination_name = f'{file_name_only}'
+##        print(destination_name)
+##        print(len(pdf_merger.pages))
+        pdf_merger.add_outline_item(destination_name, len(pdf_merger.pages))
+        
+##        pdf_merger.add_page(pdf_reader.getPage(0))
+##        print(range(len(pdf_reader.pages)))
+        # Loop through all pages in the current PDF file
+        # Remove the last page due to no meanful content
+        for page_num in range(len(pdf_reader.pages)-1):
+            pdf_merger.add_page(pdf_reader.pages[page_num])
+        
+        with open(output_pdf, 'wb') as output:
+            pdf_merger.write(output)
 
-# Print the list of files
-for file in all_files:
-    print(file)
-    # Iterate through PDF files and import to OneNote
-    #List of PDF file paths to import
-    if is_pdf_file(file):
-        pdf_file_path=file
-        merger.append(pdf_file_path)
+#main function to exeute
+if __name__ == "__main__":
+    # Specify the folder path
+##    folder_path = r'C:\Users\\Desktop\99_Temp\'
+    # Get the current working directory (current folder)
+    folder_path = os.getcwd()
 
-merger.write("merged_pdfs.pdf")
-print("Batch merge completed.")
+    # Get all files from the folder and its subfolders
+    all_files = get_files_from_folder(folder_path)
+
+    # List of PDF files to merge
+    pdf_files_to_merge =[]## ['file1.pdf', 'file2.pdf', 'file3.pdf']
+    # Print the list of files
+    for file in all_files:
+##        print(file)
+        ## List of PDF files to merge
+        if is_pdf_file(file):
+            pdf_files_to_merge.append(file)
+        
+    # Output PDF file with bookmarks
+##    output_pdf_file = 'merged_output.pdf'
+    output_pdf_file = os.path.basename(folder_path)
+    output_pdf_file = output_pdf_file + '.pdf'
+
+    merge_pdfs_with_bookmarks(pdf_files_to_merge, output_pdf_file)
+
+    print(f'Merged PDF with bookmarks saved to: {output_pdf_file}')
+
+    # Pause the script
+    input("Press Enter to continue...")
